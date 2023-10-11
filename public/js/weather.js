e183a4360fa967e3fdbe3f358e58becfa1b7faa6
Jannik Loth <jannik.loth@gmx.de>
2023-10-11 11:48:05 +0200

    add weather description to 7-day forecast

--- public/js/weather.js
+++ public/js/weather.js
@@ -3,7 +3,7 @@ console.log('weather.js loaded');
 
 
 // Event listener for the search button
-document.getElementById('searchWeatherButton').addEventListener('click', function() {
+document.getElementById('searchWeatherButton').addEventListener('click', function () {
     // log message
     console.log('searchWeatherButton clicked');
 
@@ -71,6 +71,15 @@ function showDailyWeatherData(data) {
     day5Element.querySelector('h4').textContent = `${data.daily.temperature_2m_max[4]}°C / ${data.daily.temperature_2m_min[4]}°C`;
     day6Element.querySelector('h4').textContent = `${data.daily.temperature_2m_max[5]}°C / ${data.daily.temperature_2m_min[5]}°C`;
     day7Element.querySelector('h4').textContent = `${data.daily.temperature_2m_max[6]}°C / ${data.daily.temperature_2m_min[6]}°C`;
+
+    // show weather description for every day
+    day1Element.querySelector('span').textContent = getWeatherDescription(data.daily.weathercode[0]);
+    day2Element.querySelector('span').textContent = getWeatherDescription(data.daily.weathercode[1]);
+    day3Element.querySelector('span').textContent = getWeatherDescription(data.daily.weathercode[2]);
+    day4Element.querySelector('span').textContent = getWeatherDescription(data.daily.weathercode[3]);
+    day5Element.querySelector('span').textContent = getWeatherDescription(data.daily.weathercode[4]);
+    day6Element.querySelector('span').textContent = getWeatherDescription(data.daily.weathercode[5]);
+    day7Element.querySelector('span').textContent = getWeatherDescription(data.daily.weathercode[6]);
 }
 
 // Function to display weather data on the webpage
@@ -91,4 +100,68 @@ function showWeatherData(data) {
 // Function to convert weathercode to weather icon
 function getWeatherIcon(weathercode) {
 
+}
+
+// Function to convert weathercode to weather description
+function getWeatherDescription(weathercode) {
+    switch (Number(weathercode)) {
+        case 0:
+            return 'Clear sky';
+        case 1:
+            return 'Mainly clear';
+        case 2:
+            return 'Partly cloudy';
+        case 3:
+            return 'Overcast';
+        case 45:
+            return 'Fog';
+        case 48:
+            return 'Depositing rime fog';
+        case 51:
+            return 'Light drizzle';
+        case 53:
+            return 'moderate drizzle';
+        case 55:
+            return 'Dense drizzle';
+        case 56:
+            return 'Light freezing drizzle';
+        case 57:
+            return 'Dense freezing drizzle';
+        case 61:
+            return 'Light rain';
+        case 63:
+            return 'Moderate rain';
+        case 65:
+            return 'Heavy rain';
+        case 66:
+            return 'Light freezing rain';
+        case 67:
+            return 'Heavy freezing rain';
+        case 71:
+            return 'Slight snowfall';
+        case 73:
+            return 'Moderate snowfall';
+        case 75:
+            return 'Heavy snowfall';
+        case 77:
+            return 'Snowy grains';
+        case 80:
+            return 'Light rain showers';
+        case 81:
+            return 'Moderate rain showers';
+        case 82:
+            return 'Violent rain showers';
+        case 85:
+            return 'Light snow showers';
+        case 86:
+            return 'Heavy snow showers';
+        case 95:
+            return 'Thunderstorm';
+        case 96:
+            return 'Thunderstorm with slight hail';
+        case 97:
+            return 'Thunderstorm with heavy hail';
+        default:
+            return 'Unknown';
+    }
 }
